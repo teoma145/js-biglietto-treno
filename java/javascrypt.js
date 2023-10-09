67a10f4d9f9f745fc6793211136b6d4b3bcac563
teoma145 <matteodark145@yahoo.it>
2023-10-09 17:13:52 +0200

    var-div-added-no-numbers-added

--- java/javascrypt.js
+++ java/javascrypt.js
@@ -4,10 +4,12 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo) */
-let km = prompt('Quanti km devi percorrere?');
+let miodiv = document.getElementById('testo');
+
+let km =parseInt(prompt('Quanti km devi percorrere?'))
 console.log(km);
 
-let age = prompt('Quanti anni hai?');
+let age = parseInt(prompt('Quanti anni hai?'));
 console.log(age);
 
 const minorenne = 16;
@@ -16,14 +18,18 @@ const over = 65;
 let Prezzobiglietto = (km * 0.21);
 console.log(Prezzobiglietto);
 
-if( age <= minorenne){
+
+if(isNaN(km) || isNaN(age)){
+    miodiv.innerHTML=`<p> Inserisci dei numeri <p>`
+}
+else if( age <= minorenne){
     Prezzobiglietto = Prezzobiglietto - (Prezzobiglietto * 20/100);
     Prezzobiglietto = Prezzobiglietto.toFixed(2);
-    document.getElementById('testo').innerHTML = `<p> Ciao il prezzo del tuo biglietto è ${Prezzobiglietto} euro<p>`
+    miodiv.innerHTML = `<p> Ciao il prezzo del tuo biglietto è ${Prezzobiglietto} euro<p>`
 }else if ( age >= over){
     Prezzobiglietto = Prezzobiglietto - (Prezzobiglietto * 40/100);
     Prezzobiglietto = Prezzobiglietto.toFixed(2);
-    document.getElementById('testo').innerHTML = `<p> Ciao il prezzo del tuo biglietto è ${Prezzobiglietto} euro<p>`
+    miodiv.innerHTML = `<p> Ciao il prezzo del tuo biglietto è ${Prezzobiglietto} euro<p>`
 }else{
-    document.getElementById('testo').innerHTML = `<p> Ciao il prezzo del tuo biglietto è ${Prezzobiglietto} euro<p>`
+    miodiv.innerHTML = `<p> Ciao il prezzo del tuo biglietto è ${Prezzobiglietto} euro<p>`
 }
