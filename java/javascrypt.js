c53bcf7a38007c5956ac5d3f18ae2e0d19607233
teoma145 <matteodark145@yahoo.it>
2023-10-09 16:56:01 +0200

    output-finale-con -prezzo-centesimi

--- java/javascrypt.js
+++ java/javascrypt.js
@@ -8,4 +8,22 @@ let km = prompt('Quanti km devi percorrere?');
 console.log(km);
 
 let age = prompt('Quanti anni hai?');
-console.log(age)
+console.log(age);
+
+const minorenne = 16;
+const over = 65;
+
+let Prezzobiglietto = (km * 0.21);
+console.log(Prezzobiglietto);
+
+if( age <= minorenne){
+    Prezzobiglietto = Prezzobiglietto - (Prezzobiglietto * 20/100);
+    Prezzobiglietto = Prezzobiglietto.toFixed(2);
+    document.getElementById('testo').innerHTML = `<p> Ciao il prezzo del tuo biglietto è ${Prezzobiglietto} euro<p>`
+}else if ( age >= over){
+    Prezzobiglietto = Prezzobiglietto - (Prezzobiglietto * 40/100);
+    Prezzobiglietto = Prezzobiglietto.toFixed(2);
+    document.getElementById('testo').innerHTML = `<p> Ciao il prezzo del tuo biglietto è ${Prezzobiglietto} euro<p>`
+}else{
+    document.getElementById('testo').innerHTML = `<p> Ciao il prezzo del tuo biglietto è ${Prezzobiglietto} euro<p>`
+}
